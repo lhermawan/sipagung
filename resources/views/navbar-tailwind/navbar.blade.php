ce9f6b9edda89be2a8962328e258b81a0d2037eb
Lhermawan <luckyhermawanroza@gmail.com>
2025-05-17 11:00:19 +0700

    Update navbar.blade.php

--- resources/views/navbar-tailwind/navbar.blade.php
+++ resources/views/navbar-tailwind/navbar.blade.php
@@ -28,7 +28,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="shortcut icon" href="{{URL::to('assets/img/mountain.png') }}" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.2.96/css/materialdesignicons.min.css" rel="stylesheet">
-
+    <link rel="stylesheet" href="{{ asset('build/assets/app-a0747cb1.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-d1c370f5.js') }}"></script>
     <link href="https://cdn.jsdelivr.net/npm/@mdi/font@7.3.67/css/materialdesignicons.min.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     
